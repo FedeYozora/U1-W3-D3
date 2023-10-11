c65ebd61cc863569b42f490bcc18a6b3d907a553
Federico Bonfiglio <federicobonfi@gmail.com>
2023-10-11 16:33:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// Crea un pulsante "close" e lo aggiunge alla destra degli elementi della lista
+let myNodelist = document.getElementsByTagName("LI");
+for (let i = 0; i < myNodelist.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Premere sul pulsante "close" nasconde l'elemento della lista
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    let div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+// Premere su Aggiungi permette di creare un nuovo elemnto della lista
+//  e cliccando su di esso é possibile depennarlo
+function newElement() {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("Si prega di scrivere qualcosa.");
+  } else {
+    document.getElementById("myUl").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+  li.addEventListener(
+    "click",
+    function () {
+      this.classList.toggle("checked");
+    },
+    false
+  );
+}
